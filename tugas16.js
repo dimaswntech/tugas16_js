78b620675963100f9446975377b686e21911f63a
dimaswntech <67653363+dimaswntech@users.noreply.github.com>
2021-12-21 16:09:59 +0700

    Add files via upload

---
+++ tugas16.js
@@ -0,0 +1,7 @@
+function minmax(){
+    var x = Math.min(45, 100, 99, 3, 2, 4, 1, 2, 3, 19, 22, 14, 10 );
+    var y = Math.max(45, 100, 99, 3, 2, 81, 22, 55, 1, 2, 3, 4, 5);
+    console.log("min = ",x);
+    console.log("max = ",y);
+}
+minmax();
